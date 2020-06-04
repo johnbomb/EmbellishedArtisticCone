22bb8008c9aec2262e5b43d0365773c3a190c4ea
johnny benitez <benitezjohnny95@gmail.com>
2020-06-04 00:44:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+ 
+   System.out.print("Enter a number in the range of 10 - 24: "); 
+   number = keyboard.nextInt(); 
+   while (number < 10 || numbeimport java.util.*;
+
+
+
+public class Main
+{
+   public static void main(String[] args){
+   
+   int number;
+ 
+   Scanner keyboard = new Scanner(System.in); r > 24)
+   
+    { System.out.println("That number is not in the range."); 
+   System.out.print("Enter a number in the range of 10 - 24: "); 
+   number = keyboard.nextInt(); }
+   }
+}
+
